d4788049acf686027eb1645fa5a06271fd056a59
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-06 11:11:58 +0900

    [func] func check string SameNumString

--- 230706_CheckString/230706_CheckString.cpp
+++ 230706_CheckString/230706_CheckString.cpp
@@ -8,4 +8,14 @@ public:
 	{
 		return str.size();
 	}
+
+	int CheckStringNum(const string stringA, const string stringB)
+	{
+		int ASize = stringA.size();
+		int BSize = stringB.size();
+		if (ASize == BSize)
+		{
+			return 60;
+		}
+	}
 };
