d83a0d29426c4be40ad19f3da68c7e164bdadec0
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-06 11:24:22 +0900

    [refac] func checkStringNum

--- 230706_CheckString/230706_CheckString.cpp
+++ 230706_CheckString/230706_CheckString.cpp
@@ -5,34 +5,35 @@ class StringCheck
 {
 public:
 	static const int MAX_POINT = 60;
-	int GetSize(string str)
-	{
-		return str.size();
-	}
 
 	int CheckStringNum(const string stringA, const string stringB)
 	{
-		int ASize = stringA.size();
-		int BSize = stringB.size();
-		if (ASize == BSize)
+		if (stringA.size() == stringB.size())
 		{
 			return MAX_POINT;
 		}
+		return CalcNotSameNumString(stringA, stringB);
+	}
+
+	int CalcNotSameNumString(const string stringA, const string stringB)
+	{
+		int nGap = 0;
+		int nShortSize = 0;
+		if (stringA.size() >= stringB.size())
+		{
+			nGap = stringA.size() - stringB.size();
+			nShortSize = stringB.size();
+		}
 		else
 		{
-			int Gap = 0;
-			int nShortSize = 0;
-			if (ASize >= BSize)
-			{
-				Gap = ASize - BSize;
-				nShortSize = BSize;
-			}
-			else
-			{
-				Gap = BSize - ASize;
-				nShortSize = ASize;
-			}
-			return 60 - (60 * Gap / nShortSize);
+			nGap = stringB.size() - stringA.size();
+			nShortSize = stringA.size();
 		}
+		return MAX_POINT - (MAX_POINT * nGap / nShortSize);
+	}
+
+	int GetSize(string str)
+	{
+		return str.size();
 	}
 };
